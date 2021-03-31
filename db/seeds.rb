d4d019ba4b7f50d2c24687ac8850c67b9d0d8c3e
Cássio Fagundes <cassiofagundes13@gmail.com>
2021-03-31 05:31:25 -0300

    Add pokemon.csv file and logic to seed the db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'pokemon.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'UTF-8')
+csv.each do |row|
+  pokemon = Pokemon.create({
+    pokedex_number: row['#'].to_i,
+    name: row['Name'],
+    type_1: row['Type 1'],
+    type_2: row['Type 2'],
+    total: row['Total'].to_i,
+    hp: row['HP'].to_i,
+    attack: row['Attack'].to_i,
+    defense: row['Defense'].to_i,
+    sp_atk: row['Sp. Atk'].to_i,
+    sp_def: row['Sp. Def'].to_i,
+    speed: row['Speed'].to_i,
+    generation: row['Generation'].to_i,
+    legendary: row['Legendary'].downcase == "true"
+  })
+  puts "#{pokemon.id} - #{pokemon.name} saved!"
+end
