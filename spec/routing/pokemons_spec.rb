0b2a45a261b9dcdad1cba80ca7597fb2379b41a6
Cássio Fagundes <cassiofagundes13@gmail.com>
2021-04-01 17:05:15 -0300

    Add Route and routing tests for pokemons#update and pokemons#destroy

--- spec/routing/pokemons_spec.rb
+++ spec/routing/pokemons_spec.rb
@@ -1,15 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe '/pokemons routes' do
-  it 'routes to pokemons#index' do
+  it 'should routes to pokemons#index' do
     expect(get '/pokemons').to route_to('pokemons#index')
   end
 
-  it 'routes to pokemons#show' do
+  it 'should routes to pokemons#show' do
     expect(get '/pokemons/1').to route_to('pokemons#show', id: '1')
   end
 
-  it 'routes to pokemons#create' do
+  it 'should routes to pokemons#create' do
     expect(post '/pokemons').to route_to('pokemons#create')
+  end
+
+  it 'should routes to pokemons#update' do
+    expect(put '/pokemons/1').to route_to('pokemons#update', id: '1')
+    expect(patch '/pokemons/1').to route_to('pokemons#update', id: '1')
+  end
+
+  it 'should routes to pokemon#destroy' do
+    expect(delete '/pokemons/1').to route_to('pokemons#destroy', id: '1')
   end
 end
