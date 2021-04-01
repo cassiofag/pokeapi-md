84483080de74bd6fe4a194611f9313c2d65e2e64
Cássio Fagundes <cassiofagundes13@gmail.com>
2021-04-01 17:04:29 -0300

    Add custom validation

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -11,6 +11,18 @@ class Pokemon < ApplicationRecord
     validates :speed, presence: true, numericality: { only_integer: true, greater_than: 0 }
     validates :generation, presence: true, numericality: { only_integer: true, greater_than: 0 }
     validates :legendary, :inclusion => { :in => [true, false], message: "must be true or false" }
+    validate :sum_of_stats_should_be_equal_to_total
 
     scope :pokedex_order, -> { order(pokedex_number: :asc) }
+
+    def sum_of_stats_should_be_equal_to_total
+      if total.present? && hp.present? && attack.present? && defense.present? && 
+        sp_atk.present? && sp_def.present? && speed.present?
+        
+        sum = hp + attack + defense + sp_atk + sp_def + speed
+        if sum != total
+          errors.add(:total, "should be equal to the sum of all stats")
+        end
+      end
+    end
 end
